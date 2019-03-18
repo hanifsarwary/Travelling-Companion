76260d05b40bdfb1bf5f7480beca29a12ec79d74
mianhanif13 <mianhanif13@gmail.com>
2019-03-18 13:43:06 +0500

    changes

--- UserHandling/models.py
+++ UserHandling/models.py
@@ -6,13 +6,11 @@ from django.contrib.auth.models import AbstractUser
 class Profile(AbstractUser):
     profile_pic = models.FileField(upload_to='profile pictures',null=True,blank=True)
     average_rating = models.FloatField(default=0)
-    followers = models.ManyToManyField("self",blank=True,through='FollowerFollowee')
-    following = models.ManyToManyField("self", blank=True,through='FollowerFollowee')
+    followers = models.ManyToManyField("self",blank=True,through='FollowerFollowee',symmetrical=False)
     number_of_followers = models.PositiveIntegerField(default=0)
     number_of_followings = models.PositiveIntegerField(default=0)
     bio = models.TextField(max_length=400, help_text="Enter your bio details here.")
 
 
 class FollowerFollowee(models.Model):
-    follower = models.ForeignKey(Profile,on_delete=models.CASCADE)
     followee = models.ForeignKey(Profile,on_delete=models.CASCADE)
