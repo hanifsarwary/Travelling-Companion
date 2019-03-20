c967a0404f7146e9c820ecb262d645358e02dd65
mianhanif13 <mianhanif13@gmail.com>
2019-03-20 10:03:28 +0500

    sdh

--- UserHandling/urls.py
+++ UserHandling/urls.py
@@ -3,6 +3,6 @@ from .views import *
 urlpatterns = [
     path('createprofile',CreateProfileView.as_view()),
     path('auth/', include('rest_auth.urls')),
-    path('getuserbyname/<str:name>',GetUserByName.as_view()),
+    path('getuserbyname/<str:first_name>',GetUserByName.as_view()),
     path('getuserbyusername/<str:username>',GetUserByUsername.as_view())
 ]
