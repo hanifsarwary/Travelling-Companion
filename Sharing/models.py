d0ec8b7dac52459c702d89b3873fd97595cf8ca4
mianhanif13 <mianhanif13@gmail.com>
2019-03-25 12:30:34 +0500

    changes

--- Sharing/models.py
+++ Sharing/models.py
@@ -8,26 +8,31 @@ class Service(models.Model):
     posted_by = models.ForeignKey(Profile,on_delete=models.CASCADE)
     post_time = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
+
+
+class CarSharing(Service):
     choices = (
         ('lhr', 'Lahore'),
         ('fsd', 'Faisalabad'),
         ('isb', 'Islamabad'),
     )
 
-
-class CarSharing(Service):
-
     capacity = models.PositiveIntegerField(default=1)
-    source_location = models.CharField(choices=super().choices,max_length=15)
+    source_location = models.CharField(choices=choices,max_length=15)
     dest_location = models.CharField(choices=super().choices,max_length=15)
     car_model = models.CharField(max_length=20)
     car_number = models.CharField(max_length=15)
 
 
 class LuggageSharing(Service):
+    choices = (
+        ('lhr', 'Lahore'),
+        ('fsd', 'Faisalabad'),
+        ('isb', 'Islamabad'),
+    )
 
     capacity = models.PositiveIntegerField(default=1)
-    source_location = models.CharField(choices=super().choices, max_length=15)
+    source_location = models.CharField(choices=choices, max_length=15)
     dest_location = models.CharField(choices=super().choices, max_length=15)
 
 
